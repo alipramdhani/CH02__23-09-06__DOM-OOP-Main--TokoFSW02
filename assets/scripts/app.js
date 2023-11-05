6ba4876e79efd955163c039831ff627e2bde07ea
unknown <aliframdani279@gmail.com>
2023-11-05 23:03:58 +0700

    add readme.md

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,30 +1,25 @@
 // membuat kelas
-class Product 
-{
-    constructor(title, image, desc, price) 
-    {
-        this.title = title;
-        this.imageUrl = image;
-        this.description = desc;
-        this.price = price;
-    }
-    
+class Product {
+  constructor(title, image, desc, price) {
+    this.title = title;
+    this.imageUrl = image;
+    this.description = desc;
+    this.price = price;
+  }
 }
-    
-class ProductItem 
-{
-    constructor(product) {
-        this.product = product
-    }
-    addToCart() {
-        App.addProductToCart(this.product);
-    }
-    render() 
-    {
-        const prodEl = document.createElement('li');
-     // menambahkan nama kelas pada variable prodEl
-        prodEl.className = 'product-item';
-        prodEl.innerHTML = `
+
+class ProductItem {
+  constructor(product) {
+    this.product = product;
+  }
+  addToCart() {
+    App.addProductToCart(this.product);
+  }
+  render() {
+    const prodEl = document.createElement("li");
+    // menambahkan nama kelas pada variable prodEl
+    prodEl.className = "product-item";
+    prodEl.innerHTML = `
             <div>
                 <img src="${this.product.imageUrl}" alt="${this.product.title}">
                 <div class="product-item__content">
@@ -35,105 +30,100 @@ class ProductItem
                 </div>  
             </div>
         `;
-        const addToCartButton = prodEl.querySelector("button");
-        addToCartButton.addEventListener('click', this.addToCart.bind(this));
-        return prodEl;
-    }
+    const addToCartButton = prodEl.querySelector("button");
+    addToCartButton.addEventListener("click", this.addToCart.bind(this));
+    return prodEl;
+  }
 }
-    
+
 class ShoppingCart {
-    items = [];
-    
-    set cartItem(value) {
-        this.items = value; // Perbaiki typo dari this.item menjadi this.items
-        this.totalOutput.innerHTML = `<h2>Total : Rp. ${this.totalAmount}</h2>`;
-    }
+  items = [];
 
-    get totalAmount() {
-        const sum = this.items.reduce((prevValue, curItem) => {
-            return prevValue + curItem.price;
-        }, 0);
-        return sum;
-    }
+  set cartItem(value) {
+    this.items = value; // Perbaiki typo dari this.item menjadi this.items
+    this.totalOutput.innerHTML = `<h2>Total : Rp. ${this.totalAmount}</h2>`;
+  }
 
-    addProduct(product) {
-        const updateItems = [...this.items];
-        updateItems.push(product);
-        this.cartItem = updateItems;
-    }
+  get totalAmount() {
+    const sum = this.items.reduce((prevValue, curItem) => {
+      return prevValue + curItem.price;
+    }, 0);
+    return sum;
+  }
 
-    render() {
-        const cartEl = document.createElement('section');
-        cartEl.innerHTML = `
+  addProduct(product) {
+    const updateItems = [...this.items];
+    updateItems.push(product);
+    this.cartItem = updateItems;
+  }
+
+  render() {
+    const cartEl = document.createElement("section");
+    cartEl.innerHTML = `
         <h2>Total : Rp. ${this.totalAmount}</h2>
         <button>Pesan Sekarang</button>
         `;
-        cartEl.className = 'cart';
-        this.totalOutput = cartEl.querySelector('h2');
-        return cartEl;
-    }
+    cartEl.className = "cart";
+    this.totalOutput = cartEl.querySelector("h2");
+    return cartEl;
+  }
 }
 
-class ProductList 
-{
-    products = 
-    [
-        new Product (
-            'Bantal', 
-            'https://contents.mediadecathlon.com/p1749048/k$f0b275c3207e208e12771a5c385d3ff8/camping-pillow-comfort.jpg?format=auto&quality=70&f=768x768',
-            'bantal ini lembut', 
-            10000
-            ),
-        new Product (
-            'Karpet', 
-            'https://cdn2.tstatic.net/travel/foto/bank/images/ilustrasi-karpet-terbang-aladdin.jpg',
-            'Karpet Kualitas Terbaik', 
-            80000
-            ),
-    ]
-    // method render
-    render() 
-    {
-        const renderHook = document.getElementById('app');
-        const prodList = document.createElement('ul');
-        prodList.className = 'product-list';
-        // FOR lopping 
-        for (const prod of this.products) 
-        {
-            const productItem = new ProductItem(prod);
-            const prodEl = productItem.render();
-            prodList.append(prodEl);
-        }
-        // memasukkan elementnya
-        return prodList;
+class ProductList {
+  products = [
+    new Product(
+      "Bantal",
+      "https://inthebox.sgp1.cdn.digitaloceanspaces.com/public/posts/ukuran-bantal-inthebox.jpg",
+      "bantal ini lembut",
+      10000
+    ),
+    new Product(
+      "Karpet",
+      "https://www.internalgroup.id/files/product_pictures/kpt-04-lady-rose-karpet-150x190-new-motif-1675066952_thumbnail1280x1280.jpg",
+      "Karpet Kualitas Terbaik",
+      80000
+    ),
+  ];
+  // method render
+  render() {
+    const renderHook = document.getElementById("app");
+    const prodList = document.createElement("ul");
+    prodList.className = "product-list";
+    // FOR lopping
+    for (const prod of this.products) {
+      const productItem = new ProductItem(prod);
+      const prodEl = productItem.render();
+      prodList.append(prodEl);
     }
-}     
-    
+    // memasukkan elementnya
+    return prodList;
+  }
+}
+
 class FSW2Shop {
-    render(){
-        const renderHook = document.getElementById('app');
+  render() {
+    const renderHook = document.getElementById("app");
 
-        this.cart = new ShoppingCart();
-        const cartEl = this.cart.render();
+    this.cart = new ShoppingCart();
+    const cartEl = this.cart.render();
 
-        const productList = new ProductList();
-        const prodListEl = productList.render();
+    const productList = new ProductList();
+    const prodListEl = productList.render();
 
-        renderHook.append(cartEl);
-        renderHook.append(prodListEl);
-    }
+    renderHook.append(cartEl);
+    renderHook.append(prodListEl);
+  }
 }
 // static method
 class App {
-    static init(){
-        const shop = new FSW2Shop();
-        shop.render();
-        this.cart = shop.cart;
-    }
+  static init() {
+    const shop = new FSW2Shop();
+    shop.render();
+    this.cart = shop.cart;
+  }
 
-    static addProductToCart(product) {
-        this.cart.addProduct(product);
-    }
+  static addProductToCart(product) {
+    this.cart.addProduct(product);
+  }
 }
 App.init();
-
---
+++ readme.md
@@ -0,0 +1,15 @@
+# Aplikasi FSW02 Shop menggunakan Javascript dan DOM
+
+### 1. Clone This Repository
+
+```shell
+git clone https://github.com/alipramdhani/CH02__23-09-06__DOM-OOP-Main--TokoFSW02.git
+```
+
+### 2. Open Browser on this file HTML
+
+### 3. Play FSW02 Shop Application!
+
+### FSW02 Shop App
+
+![DrumkitApp](/assets/images/FSW2Shop-App-Image.png)
